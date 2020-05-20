d85c273f4ec55a03724c6bf9aaa52c8567e58455
Suzee1993 <Suzee1993@gmail.com>
2020-05-20 20:40:30 +0200

    Shoot, Bullet

---
+++ Binding of Isaac/Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float lifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Delay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSecondsRealtime(lifetime);
+        Destroy(gameObject);
+    }
+}
