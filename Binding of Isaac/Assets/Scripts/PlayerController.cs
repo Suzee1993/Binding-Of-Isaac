9f90cc29e5a136278126644e6ae107b61bec1b24
Suzee1993 <Suzee1993@gmail.com>
2020-05-20 20:04:12 +0200

    Assets PlayerMovement Collection

---
+++ Binding of Isaac/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    public Text collectedText;
+
+    public static int collectedAmount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
+
+        collectedText.text = "Items Collected: " + collectedAmount;
+        
+    }
+}
