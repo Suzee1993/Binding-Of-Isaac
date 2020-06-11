e679b4f8a980a2f634d8d29bc3cdbba6f5159071
Suzee1993 <58231438+Suzee1993@users.noreply.github.com>
2020-06-11 16:18:22 +0200

    See Description
    Started working on the Dungeon Generator
    
    Made a start room
    
    Made am Empty room

---
+++ Binding of Isaac/Assets/Scripts/Dungeon/Room.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public float width;
+    public float height;
+    public int x;
+    public int y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(RoomController.Instance == null)
+        {
+            Debug.Log("You pressed play in de wrong scene.");
+            return;
+        }
+
+        RoomController.Instance.RegisterRoom(this);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0)); 
+    }
+
+    public Vector3 GetRoomCenter()
+    {
+        return new Vector3(x * width, y * height);
+    }
+}
---
+++ Binding of Isaac/Assets/Scripts/Dungeon/RoomController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomInfo
+{
+    public string name;
+    public int xVal;
+    public int yVal;
+}
+
+public class RoomController : MonoBehaviour
+{
+    public static RoomController Instance;
+
+    string currentWorldName = "Basement";
+
+    RoomInfo currentLoadRoomData;
+
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+
+    public List<Room> loadedRooms = new List<Room>();
+
+    bool isLoadingRoom = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        LoadRoom("Start", 0, 0);
+        LoadRoom("Empty", 1, 0);
+        LoadRoom("Empty", -1, 0);
+        LoadRoom("Empty", 0, 1);
+        LoadRoom("Empty", 0, -1);
+    }
+
+    private void Update()
+    {
+        UpdateRoomQueue();
+    }
+
+    void UpdateRoomQueue()
+    {
+        if (isLoadingRoom)
+        {
+            return;
+        }
+
+        if(loadRoomQueue.Count == 0)
+        {
+            return;
+        }
+
+        currentLoadRoomData = loadRoomQueue.Dequeue();
+        isLoadingRoom = true;
+        StartCoroutine(LoadRoomRoutine(currentLoadRoomData));
+    }
+
+    public void LoadRoom(string sceneName, int x, int y)
+    {
+        if (DoesRoomExist(x, y))
+        {
+            return;
+        }
+
+        RoomInfo newRoomData = new RoomInfo();
+
+        newRoomData.name = sceneName;
+        newRoomData.xVal = x;
+        newRoomData.yVal = y;
+
+        loadRoomQueue.Enqueue(newRoomData);
+    }
+
+    public void RegisterRoom(Room room)
+    {
+        room.transform.position = new Vector3(currentLoadRoomData.xVal * room.width, currentLoadRoomData.yVal * room.height, 0);
+
+        room.x = currentLoadRoomData.xVal;
+        room.y = currentLoadRoomData.yVal;
+        room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.x + "," + room.y;
+        room.transform.parent = transform;
+
+        isLoadingRoom = false;
+
+        loadedRooms.Add(room);
+    }
+
+    public bool DoesRoomExist(int x, int y)
+    {
+        return loadedRooms.Find(item => item.x == x && item.y == y) != null;
+    }
+
+    IEnumerator LoadRoomRoutine(RoomInfo info)
+    {
+        string roomName = currentWorldName + info.name;
+
+        //Async operation
+        //LoadSceneMode.Additive = So the scenes will overlap and the rooms will be put in the same scene so I can use/access them
+        AsyncOperation loadRoom = SceneManager.LoadSceneAsync(roomName, LoadSceneMode.Additive);
+
+        while(loadRoom.isDone == false)
+        {
+            yield return null;
+        }
+    }
+
+
+
+}
